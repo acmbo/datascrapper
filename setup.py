680cd7558cb6766511fc38692c1c97384ac5c4ef
Stephan Wegewitz <wegewitzstephan@gmail.com>
2021-09-18 12:28:59 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='datascrapper',
+      version='0.5',
+      description='Python Scrapper for DW',
+      author='acmbo',
+      author_email='',
+      url='',
+      packages=['pymongo', 
+                'random',
+                'urllib',
+                'beautifulsoup4',
+                'torpy'],
+     )
