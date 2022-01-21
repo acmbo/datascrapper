27a5ed7d116fba234b4039848ba8427a4150318e
Stephan Wegewitz <wegewitzstephan@gmail.com>
2022-01-21 19:43:17 +0100

    "Further code for article test"

--- test/test_article.py
+++ test/test_article.py
@@ -31,4 +31,85 @@ else:
 art = get_empty_article_meta_data()
 art['url'] = url
 test = extract_article_data(art, html)
-
+empty_entrys = 0
+correct_entrys = 0
+ 
+for key in ['url', 'title', 'subtitle', 'abstract', 'image_title', 'mainarticle', 'video', 'meistgelesen', 'redaktionsempfehlung', 'Datum', 'Autorin/Autor', 'Permalink', 'Themenseiten', 'Schlagwörter', 'Artikel']:
+    if key == 'url':
+        if type(test[key]) == str:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'title':
+        if type(test[key]) == str:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'subtitle':
+        if type(test[key]) == str:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'abstract':
+        if type(test[key]) == str:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'image_title':
+        if  type(test[key]) == str:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'mainarticle':
+        if type(test[key]) == bool:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'video':
+        if type(test[key]) == bool:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'meistgelesen':
+        if type(test[key]) == bool:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'redaktionsempfehlung':
+        if type(test[key]) == bool:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'Datum':
+        if type(test[key]) == str:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'Autorin/Autor':
+        if type(test[key]) == str:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'Permalink':
+        if type(test[key]) == str:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'Themenseiten':
+        if type(test[key]) == list:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'Schlagwörter':
+        if type(test[key]) == list:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+    if key == 'Artikel':
+        if type(test[key]) == dict:
+            correct_entrys += 1
+        else:
+            empty_entrys +=1 
+            
+print("Correct Entrys: ", correct_entrys)
+print("Empty Entrys: ", empty_entrys)
