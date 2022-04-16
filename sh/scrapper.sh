df77fdd4665dd396d9daad52b76fadfbd4325f65
acmbo <wegewitz.stephan@gmail.com>
2022-04-16 15:53:05 +0200

    Remove files not needed

--- sh/scrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo redis-server /home/pi/programming/datascrapper/src/db/redis.conf
-
-python datascrapper/src/slug.py
--- src/db/redis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in ./*.py ; do 
-	echo "Diese Datei: $file"
-	fname=$(basename "$file")
-	echo "hat den Namen: $fname"
-	fdir=$(dirname "$file")
-	echo "und steht im Verzeichnis: $fdir"
-done
